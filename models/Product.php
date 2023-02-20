aa8b390920a121395acb557d61073959dc1d6672
FedericoGuadagnoli <guadagnoli1989@gmail.com>
2023-02-20 16:50:58 +0100

    Add functions get in Product

--- models/Product.php
+++ models/Product.php
@@ -1,14 +1,33 @@
 <?php
 class Product
 {
+    public $image;
     public $name;
     public $description;
     public $prize;
 
-    public function __construct($name, $description, $prize)
+    public function __construct($image, $name, $description, $prize)
     {
         $this->name = $name;
         $this->description = $description;
         $this->prize = $prize;
+        $this->image = $image;
+    }
+
+    public function getImage()
+    {
+        return "$this->image";
+    }
+    public function getName()
+    {
+        return "$this->name";
+    }
+    public function getDescription()
+    {
+        return "$this->description";
+    }
+    public function getPrize()
+    {
+        return "$this->prize";
     }
 }
